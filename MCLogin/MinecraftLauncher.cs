6a6937a29656c1f09674a615caef27b7fd0749ac
yair vilner <yair.vilner@gmail.com>
2024-08-05 00:38:30 +0300

    launch minecraft - part 2

--- MCLogin/MinecraftLauncher.cs
+++ MCLogin/MinecraftLauncher.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Text.Json;
 
@@ -27,15 +29,46 @@ public class MinecraftLauncher
     public void LaunchMinecraft(Dictionary<string, string> args)
     {
         using StreamReader jsonStream = new(Path.Combine(_minecraftPath.FullName, "versions", _version, _version + ".json"));
-        var launchArgsJson = JsonDocument.Parse(jsonStream.ReadToEnd()).RootElement.GetProperty("arguments");
-        StringBuilder cmdCommandBuilder = new($"/C {Path.Combine(_minecraftPath.FullName, "runtime", "java-runtime-delta", )}");
-    }
-    private string GetOSAndArchitecture()
-    {
-        if (Environment.Is64BitOperatingSystem)
+        var versionJson = JsonDocument.Parse(jsonStream.ReadToEnd());
+        var launchArgsJson = versionJson.RootElement.GetProperty("arguments");
+        string javaComponent  = versionJson.RootElement.GetProperty("javaVersion").GetProperty("component").GetString();
+        StringBuilder cmdCommandBuilder = new($"/C {Path.Combine(_minecraftPath.FullName, "runtime", javaComponent, GetJvmPlatformName(), javaComponent, "bin", "java.exe")}");
+        foreach (var arg in launchArgsJson.GetProperty("game").EnumerateArray())
         {
-            if (OperatingSystem.IsWindows())
-                return 
+            if (arg.ValueKind == JsonValueKind.Object)
+            {
+                bool isRulesMatch = true;
+                foreach (var rule in arg.GetProperty("rules").EnumerateArray())
+                {
+                    
+                }
+            }
         }
+    }
+    private static bool IsRulesMatch(JsonDocument rules) => true;
+    private static string GetJvmPlatformName()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if (Environment.Is64BitOperatingSystem)
+                return "windows-x64";
+            else
+                return "windows-x86";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            if (Environment.Is64BitOperatingSystem)
+                return "linux";
+            else
+                return "linux-i386";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+                return "mac-os-arm64";
+            else
+                return "mac-os";
+        }
+        else return "gamecore";
     }
 }
